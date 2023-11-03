1e931498037b10db943f9697dd56145b2e963143
Misha <mishagrigoryan@gmail.com>
2023-11-02 23:59:07 -0400

    2

--- toBlob.py
+++ toBlob.py
@@ -8,9 +8,12 @@ import os
 # blob_name = ".csv" 
 
 file_repo_path = "C:\\Users\\Misha\\Desktop\\GitHub\\reddit_to_azure_postgres"
-file_name = "sample.csv"
-file_path = os.path.join(file_repo_path, file_name)
+original_file_name = "sample.csv"
+original_file_path = os.path.join(file_repo_path, original_file_name)
 
-df = pd.read_csv(file_path)
+df = pd.read_csv(original_file_path)
 
-df
+current_datetime = datetime.now().strftime("%Y%m%d_%H%M%S")
+new_file_name = f"sample_{current_datetime}.csv"
+new_file_path = os.path.join(file_repo_path, new_file_name)
+
