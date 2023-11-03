aec6d69c718aca13942b8e3e4f94da6b99c6beb7
Misha <mishagrigoryan@gmail.com>
2023-11-02 23:59:49 -0400

    3

--- toBlob.py
+++ toBlob.py
@@ -17,3 +17,14 @@ current_datetime = datetime.now().strftime("%Y%m%d_%H%M%S")
 new_file_name = f"sample_{current_datetime}.csv"
 new_file_path = os.path.join(file_repo_path, new_file_name)
 
+df.to_csv(new_file_path, index=False)
+
+blob_name = new_file_name
+
+# blob_service_client = BlobServiceClient.from_connection_string(connect_str)
+# blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
+
+with open(new_file_path, "rb") as data:
+    blob_client.upload_blob(data, overwrite=True)
+
+print(f"The file '{new_file_name}' has been uploaded to Azure Blob Storage.")
