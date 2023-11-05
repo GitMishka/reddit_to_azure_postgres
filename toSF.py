ef64c430f0534b83a709e5e61f40b7c4fce00671
Misha <mishagrigoryan@gmail.com>
2023-11-04 20:14:59 -0400

    snowflake code

--- toSF.py
+++ toSF.py
@@ -1,3 +1,9 @@
-# CREATE OR REPLACE STAGE your_stage_name
-# URL='azure://<storage-account-name>.blob.core.windows.net/<container-name>'
-# CREDENTIALS=(AZURE_SAS_TOKEN='<your-sas-token>');
+CREATE OR REPLACE STAGE your_stage_name
+URL='azure://<storage-account-name>.blob.core.windows.net/<container-name>'
+CREDENTIALS=(AZURE_SAS_TOKEN='<your-sas-token>');
+
+
+
+COPY INTO your_target_table
+FROM @your_stage_name/file_prefix  -- if your files have a prefix, otherwise just use the stage name
+FILE_FORMAT = (TYPE = 'CSV' FIELD_OPTIONALLY_ENCLOSED_BY = '"' SKIP_HEADER = 1);
