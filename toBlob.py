4fc81d2a58ec30ee553f7e368f961cd94e7f7fd4
Misha <mishagrigoryan@gmail.com>
2023-11-12 16:33:33 -0500

    clean up

--- toBlob.py
+++ toBlob.py
@@ -2,16 +2,18 @@ import pandas as pd
 from azure.storage.blob import BlobServiceClient
 from datetime import datetime
 import os
+import glob
 import config
 
 connect_str = config.github1
 container_name = "github1"
 
 file_repo_path = "C:\\Users\\Misha\\Desktop\\GitHub\\reddit_to_azure_postgres"
-original_file_name = "sample.csv"
-original_file_path = os.path.join(file_repo_path, original_file_name)
 
-df = pd.read_csv(original_file_path)
+list_of_files = glob.glob(os.path.join(file_repo_path, '*.csv')) #
+latest_file = max(list_of_files, key=os.path.getmtime)
+
+df = pd.read_csv(latest_file)
 
 current_datetime = datetime.now().strftime("%Y%m%d_%H%M%S")
 new_file_name = f"data_{current_datetime}.csv"
@@ -20,7 +22,6 @@ new_file_path = os.path.join(file_repo_path, new_file_name)
 df.to_csv(new_file_path, index=False)
 
 blob_name = new_file_name
-
 blob_service_client = BlobServiceClient.from_connection_string(connect_str)
 blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
 
